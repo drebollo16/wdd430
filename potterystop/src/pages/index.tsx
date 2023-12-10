14eeef3e8d866acf0aac6d86b409985a05aa7276
Denise Rebollo <drebollo16@byui.edu>
2023-12-09 21:03:23 -0700

    change

--- potterystop/src/pages/index.tsx
+++ potterystop/src/pages/index.tsx
@@ -29,38 +29,7 @@ export default function Home() {
         <section className="mb-8 mx-auto flex justify-center">
           <img src="/images/art1.jpg" alt="art image" className="w-250 h-90 object-cover mb-4 rounded-md" /> 
         </section>
-
-        <section className="mb-8 m-20">
-  <h2 className="text-2xl font-bold text-gray-800 mb-4">Upcoming Events</h2>
-
-  <div className="flex items-center">
-    <img
-      src="/images/artwinter.jpg"
-      alt="Event 1"
-      className="w-20 h-20 object-cover rounded-md mr-4"
-    />
-    <div>
-      <h3 className="text-lg font-bold">Event 1: Craft Workshop</h3>
-      <p className="text-gray-700">Date: January 15, 2024</p>
-      <p className="text-gray-700">Location: CraftStore Workshop Space</p>
-    </div>
-  </div>
-
-  <div className="flex items-center mt-4">
-    <img
-      src="/images/artmtns.png"
-      alt="Event 2"
-      className="w-20 h-20 object-cover rounded-md mr-4"
-    />
-    <div>
-      <h3 className="text-lg font-bold">Event 2: Art Exhibition</h3>
-      <p className="text-gray-700">Date: February 5, 2024</p>
-      <p className="text-gray-700">Location: Art Gallery</p>
-    </div>
-  </div>
-
-  {/* Add more events as needed */}
-</section>
+ 
       </main> 
     
  
